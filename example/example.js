4d07a07a665bfebbae06103ca4f1b39fb5036356
서준 <105553946+seojun0602@users.noreply.github.com>
2025-07-20 18:24:08 +0900

    example.js 업데이트

--- example/example.js
+++ example/example.js
@@ -1,10 +1,16 @@
 let {dh2md} = require("DynamicHtml2md");
 
 let url = "https://ccentury.dothome.co.kr/Ccentury/DTD/rank/Playlog.php";
+
+let options = {
+    maxwt: 10000,
+    timeout: 30000
+};
+
 dh2md(url, function(error, html) {
     if (error) {
         console.error(error.message);
         return;
     }
     console.log(html);
-});
+}, options);

5ce1fa80ef7166c08f88b65d3d25f3a76c2cb50d
서준 <105553946+seojun0602@users.noreply.github.com>
2025-07-24 13:52:18 +0900

    Update example.js
    add examples for options

--- example/example.js
+++ example/example.js
@@ -1,10 +1,18 @@
-let {dh2md, getHtml} = require("DynamicHtml2md");
+let {dh2md, getHtml, Cookie} = require("DynamicHtml2md");
 
 let url = "https://www.kobis.or.kr/kobis/business/main/main.do";
 
+// cookies 예시.
+let cookies = [
+    new Cookie("a", "1").setPath("/"),
+    new Cookie("b", "2").setDomain("example.com"),
+];
+
 let options = {
     maxwt: 10000,
-    timeout: 30000
+    timeout: 30000,
+    userAgent: "KT/25.6.5",
+    cookies: cookies
 };
 
 dh2md(url, function(error, html) {
